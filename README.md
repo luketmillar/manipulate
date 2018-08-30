80b936447f3b64f1c1a2b226a67b5d04ecbd50d3
Luke Millar <luketmillar@gmail.com>
2018-08-30 10:56:11 -0700

    Update tests and README

--- README.md
+++ README.md
@@ -1,3 +1,123 @@
-# Brass Tacks
+# Manipulate
 
-This is a grab bag of javascript helper functions and React components to get projects up and running quicker
+Manipulate exposes standard array manipulation functions (`filter`, `map`, `reduce` and `forEach`) on your JS objects.
+
+# Installation
+
+Using npm:
+
+```
+$ npm i --save manipulate
+```
+
+# Usage
+
+Example Input Object
+
+```
+const usersById = {
+  ltm: {
+    name: 'Luke Millar',
+    isAdmin: true,
+    followers: 2496
+  },
+  dshankar: {
+    name: 'Darshan Shankar',
+    isAdmin: true,
+    followers: 8472
+  },
+  jazzychad: {
+    name: 'Chad Etzel',
+    isAdmin: false,
+    followers: 4759
+  }
+}
+```
+
+## Filter
+
+```
+const Manipulate = require('manipulate')
+
+Manipulate(usersById)
+    .filter(({ value }, index, usersById) => value.isAdmin)
+    .value()
+
+/*
+RESULT
+{
+  ltm: {
+    name: 'Luke Millar',
+    isAdmin: true,
+    followers: 2496
+  },
+  dshankar: {
+    name: 'Darshan Shankar',
+    isAdmin: true,
+    followers: 8472
+  }
+}
+*/
+```
+
+## Map
+
+```
+const Manipulate = require('manipulate')
+
+Manipulate(usersById)
+    .map(({ key, value }, index, usersById) => value.name)
+    .value()
+
+/*
+RESULT
+{
+    ltm: 'Luke Millar',
+    dshankar: 'Darshan Shankar',
+    jazzychad: 'Chad Etzel'
+}
+*/
+```
+
+## Reduce
+
+```
+const Manipulate = require('manipulate')
+
+Manipulate(usersById)
+    .reduce((totalFollowers, { key, value }, index, usersById) => totalFollowers + value.followers, 0)
+    .value()
+
+/*
+RESULT
+15727
+*/
+```
+
+## ForEach
+
+```
+const Manipulate = require('manipulate')
+
+Manipulate(usersById)
+    .forEach(({ key, value }) => {
+        console.log(key)
+    })
+```
+
+## Chaining
+
+```
+const Manipulate = require('manipulate')
+
+Manipulate(usersById)
+      .filter(({ value }) => value.isAdmin)
+      .map(({ value }) => value.followers)
+      .reduce((totalFollowers, { value: followers }) => totalFollowers + followers, 0)
+      .value()
+
+/*
+RESULT
+10968
+*/
+```
--- test/object/index.test.js
+++ test/object/index.test.js
@@ -1,46 +1,68 @@
-import { Iterable } from ''
+import { Manipulate } from ''
 
-test('Iterable.value', () => {
-  const a = { b: 1 }
-  expect(Iterable(a).value()).toBe(a)
+const usersById = {
+  ltm: {
+    name: 'Luke Millar',
+    isAdmin: true,
+    followers: 2496
+  },
+  dshankar: {
+    name: 'Darshan Shankar',
+    isAdmin: true,
+    followers: 8472
+  },
+  jazzychad: {
+    name: 'Chad Etzel',
+    isAdmin: false,
+    followers: 4759
+  }
+}
+
+test('Manipulate.value', () => {
+  expect(Manipulate(usersById).value()).toBe(usersById)
 })
 
-test('Iterable.map', () => {
-  const obj = { a: 1, b: 2, c: 2, d: 3 }
+test('Manipulate.map', () => {
   expect(
-    Iterable(obj)
-      .map(({ value }) => value + 1)
+    Manipulate(usersById)
+      .map(({ value: user }) => user.name)
       .value()
-  ).toEqual({ a: 2, b: 3, c: 3, d: 4 })
-  expect(
-    Iterable(obj)
-      .map(({ value }) => value + 1)
-      .map(({ key, value }) => `${key}-${value}`)
-      .value()
-  ).toEqual({ a: 'a-2', b: 'b-3', c: 'c-3', d: 'd-4' })
+  ).toEqual({ ltm: 'Luke Millar', dshankar: 'Darshan Shankar', jazzychad: 'Chad Etzel' })
 })
 
-test('Iterable.filter', () => {
-  const obj = { a: 1, b: 2, c: 2, d: 3 }
+test('Manipulate.filter', () => {
   expect(
-    Iterable(obj)
-      .filter(({ value }) => value === 2)
+    Manipulate(usersById)
+      .filter(({ value: user }) => user.isAdmin)
       .value()
-  ).toEqual({ b: 2, c: 2 })
-  expect(
-    Iterable(obj)
-      .filter(({ key }) => key === 'a')
-      .value()
-  ).toEqual({ a: 1 })
+  ).toEqual({
+    ltm: {
+      name: 'Luke Millar',
+      isAdmin: true,
+      followers: 2496
+    },
+    dshankar: {
+      name: 'Darshan Shankar',
+      isAdmin: true,
+      followers: 8472
+    }
+  })
 })
 
-test('Iterable combination', () => {
-  const obj = { int1: 1, int2: 2, int3: 2, int4: 3, str1: 'a', str2: 'b', str3: 'c' }
+test('Manipulate.reduce', () => {
   expect(
-    Iterable(obj)
-      .filter(({ key }) => key.startsWith('int'))
-      .map(({ value }) => value + 1)
-      .reduce((result, entry) => result + entry.value, 0)
+    Manipulate(usersById)
+      .reduce((totalFollowers, { value: user }) => totalFollowers + user.followers, 0)
       .value()
-  ).toEqual(12)
+  ).toEqual(15727)
+})
+
+test('Manipulate combination', () => {
+  expect(
+    Manipulate(usersById)
+      .filter(({ value }) => value.isAdmin)
+      .map(({ value }) => value.followers)
+      .reduce((totalFollowers, { value: followers }) => totalFollowers + followers, 0)
+      .value()
+  ).toEqual(10968)
 })
